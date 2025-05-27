c92bea388d238832652e6681ff340d3bdebdaa7d
Dineth <dinethfiverr23@gmail.com>
2025-05-27 14:51:13 +0530

    Add Animation To Services Section

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -1,39 +1,59 @@
 import React from 'react'
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
+import { motion } from "motion/react"
 
 function Services() {
     return (
 
-        <div
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
             id="work"
             className="w-full px-4 sm:px-10 lg:px-[12%] py-20 scroll-mt-2  text-slate-800"
         >
 
             <div className="text-center mb-12">
 
-                <h4 className="text-green-600 text-lg font-Ovo ">What I Offer</h4>
+                <motion.h4
+                    initial={{ y: -20, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ delay: 0.3, duration: 0.5 }}
+                    className="text-green-600 text-lg font-Ovo ">What I Offer</motion.h4>
 
-                <h2 className="text-2xl sm:text-4xl font-bold mt-2 dark:text-gray-300">My Services</h2>
+                <motion.h2
+                    initial={{ y: -20, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ delay: 0.5, duration: 0.5 }}
+                    className="text-2xl sm:text-4xl font-bold mt-2 dark:text-gray-300">My Services</motion.h2>
 
-                <p className="text-center max-w-2xl mx-auto mt-5 mb-12 text-gray-600 text-base
+                <motion.p
+                    initial={{ y: -20, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{ delay: 0.7, duration: 0.5 }}
+                    className="text-center max-w-2xl mx-auto mt-5 mb-12 text-gray-600 text-base
                 dark:text-gray-300">
 
                     I provide a wide range of modern development services, focusing on performance, accessibility, and design excellence.
 
-                </p>
+                </motion.p>
 
             </div>
 
             {/* Services Grid */}
 
-            <div
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ delay: 0.9, duration: 0.6 }}
                 className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))]
                 gap-8 m-10"
             >
                 {serviceData.map(({ icon, title, description, link }, index) => (
 
-                    <div
+                    <motion.div
+                        whileHover={{ scale: 1.05 }}
                         key={index}
                         className="border border-gray-200 rounded-xl px-6 py-10 bg-white shadow-sm 
                         hover:shadow-md hover:-translate-y-1 transition-all duration-300 cursor-pointer 
@@ -65,13 +85,14 @@ function Services() {
 
                         </a>
 
-                    </div>
+                    </motion.div>
 
                 ))}
 
-            </div>
+            </motion.div>
 
-            <a
+            <motion.a
+                whileHover={{ scale: 1.05 }}
                 href=""
                 className="flex items-center justify-center gap-2 text-sm font-medium text-slate-700 border border-slate-400
                                 rounded-full py-3 px-8 mx-auto mt-16 hover:bg-gray-700 dark:text-green-500
@@ -79,9 +100,9 @@ function Services() {
             >
                 View Projects
                 <Image src={assets.rightArrow} alt="right arrow" className="w-4" />
-            </a>
+            </motion.a>
 
-        </div>
+        </motion.div>
 
     );
 
