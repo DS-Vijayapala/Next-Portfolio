585f6c78e3b3ba174a194549e66fc39a5cb673a1
Dineth <dinethfiverr23@gmail.com>
2025-10-05 21:47:09 +0530

    Attact Hero Section To Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 
 import React from "react";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import About from "../components/About";
 import Project from "../components/Project";
 import Contact from "../components/Contact";
+import Hero from "../components/Header/Header";
 
 
 export default function Home() {
 
   return (
     <>
-      <Header />
+      <Hero />
       {/* <About isDarkMode={false} />
       <Project />
       <Contact /> */}
